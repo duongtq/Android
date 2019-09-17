d57f22288437a5915e733534835a1994a48b09ee
Tran Quy Duong <duong.tq.dev@gmail.com>
2019-09-17 22:56:37 +0700

    Pause CriminalIntent for a while

--- CriminalIntent/app/src/main/java/com/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/android/criminalintent/CrimeActivity.java
@@ -1,10 +1,6 @@
 package com.android.criminalintent;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
